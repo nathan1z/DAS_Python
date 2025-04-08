5182280c06ce25e5a79fb623a8a478abf25575ff
nathan1z <jznathan01@gmail.com>
2025-04-07 22:58:33 -0500

    DayOne

---
+++ dayOne.py
@@ -0,0 +1,22 @@
+
+print(2 + 3)            
+print(3 - 1)           
+print(2 * 3)           
+print(3 / 2)             
+print(3 ** 2)           
+print(3 % 2)            
+print(3 // 2)          
+
+print(type(10))          
+print(type(3.14))         
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  
+print(type([1, 2, 3]))   
+print(type({'name':'Asabeneh'})) 
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+print("Euclidean distance")
+#(2,3),#(10,8)
+print((3-2)**2+(8-10)**2)
+
